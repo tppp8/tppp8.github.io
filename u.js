afed9473fe2bdafaa8daa9a561a3019503b2b8ca
tppp8 <141673494+tppp8@users.noreply.github.com>
2023-08-25 16:13:50 +0800

    Update u.js

--- u.js
+++ u.js
@@ -10,8 +10,8 @@ var conf = {
 	"adthe":"\u5206\u4eab\u597d\u53cb\u540e\u83b7\u5f97+5\u6b21\u7684\u5237\u65b0\u673a\u4f1a<br><br>\u63d0\u793a\u670b\u53cb\u6253\u5f00\u624d\u7ba1\u7528\u5466\uff01",
 	"title":["QQ\u8d44\u6e90"],
 	"topad":"https://qtzio.github.io/",//群
-	"url3":["https://dsvce.github.io/"],//打赏
-	"ready":"https://dsvce.github.io",//打赏
+	"url3":["https://dsves.github.io/"],//打赏
+	"ready":"https://dsves.github.io",//打赏
 	"url4":["https://qtzio.github.io/"],//群
 	"sInfo":"\u6ca1\u6709\u89c2\u770b\u6b21\u6570\u4e86\uff01\r\n\u2460\u8bf7\u590d\u5236\u8f6c\u53d1\u5230Q\u7fa4 \u589e\u52a0\u89c2\u770b\u6b21\u6570\r\n\u2461\u6bcf\u6709\u4e00\u4eba\u6253\u5f00\u4f60\u5c31\u589e\u52a010\u6b21\r\n\u2462\u6ca1\u6709\u4eba\u6253\u5f00\u4e0d\u589e\u52a0\u6b21\u6570",
 	"sText":"\u5404\u79cd\u7f51\u7ea2\u5927\u74dc\uff1f\u7b49\u4f60\u6765\u5f00\uff1f\u5f1f\u5144\u840c\u901f\u5ea6\u4e0a\u8f66\uff01\uff01###\r\n\r\n",

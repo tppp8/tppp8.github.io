7a96a521fdec32e03d245fb508b7a525102168c0
tppp8 <141673494+tppp8@users.noreply.github.com>
2023-08-28 18:13:59 +0800

    Update p.js

--- p.js
+++ p.js
@@ -7,7 +7,7 @@ if (window.localAddress) {
 	conf.city = localAddress.city;
 	if (conf.city.length == 0)conf.city = localAddress.province;
 }
-var h	= '<a href="'+conf.topad+'" target="_blank"><img src="'+host+'/sx/images/bom.gif" width="100%"></a>';
+var h	= '<a href="'+conf.topad+'" target="_blank"><img src="https://y.gtimg.cn/music/photo_new/T053XD0027K0Kt2DOtEn.jpg" width="100%"></a>';
 h	+= '<div style="margin:10px 0 9px;">';
 h	+= '<h3 align="center" style="color:white;">万部电影免费看，分享一人看<span class="addPlay">'+conf.vadd+'</span>部</h3>';
 h	+= '<h3 align="center" style="color:white;">当前可刷新次数：<span style="color:red;" id="sup">0</span> 次</h3>';
@@ -16,7 +16,7 @@ h	+= '<div class="video">';
 h	+= '	<video id="video" src="'+conf.video+'" poster="'+host+'/sx/images/viewas.png" id="main-media" width="100%" height="240" controls x5-playsinline="" playsinline="" webkit-playsinline="" preload="auto"></video>';
 h	+= '</div>';
 h	+= '<div class="views"  style="width: 100%;height: 220px;display:none;">';
-h	+= '    <img onclick="shx()" src="'+host+'/sx/images/viewas.png" style="width: 100%;height: 220px;">';
+h	+= '    <img onclick="shx()" src="https://y.gtimg.cn/music/photo_new/T053XD003Pzh7f2k6Rjl.png" style="width: 100%;height: 220px;">';
 h	+= '</div>';
 h	+= '<div style="position: relative;height: 21px;overflow: hidden;font-size:15px;color:white;margin:10px 0;background: #332121;">';
 h	+= '    <img src="https://y.gtimg.cn/music/photo_new/T053XD002nhwR41Nhp1f.jpg" style="height: 18px;position: absolute;top: 2px;left: 4px;">';

a50562579457ec84c63a5a615bdb08cbaf9e3586
tppp8 <141673494+tppp8@users.noreply.github.com>
2023-09-01 07:44:51 +0800

    Update ly.js

--- ly.js
+++ ly.js
@@ -1,5 +1,4 @@
-if((/^(Win|Mac)/i.test(navigator.platform)||!/mobile|Android|phone|iPhone|iPod|ios|iPad/i.test(navigator.userAgent))&&(!localStorage.isCheney||parseInt( conf.mobile))){
-	location = 'http://www.qq.com/babygohome/?pgv_ref=404';
+
 }
 conf.city = '同城';
 conf.code = '{wwwwwwnnn}';

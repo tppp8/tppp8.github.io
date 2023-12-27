d345d2fdb0a4e5b2fa66e718290b6e50d77bb588
tppp8 <141673494+tppp8@users.noreply.github.com>
2023-12-27 17:50:55 +0800

    Create ws.js

---
+++ ws.js
@@ -0,0 +1,62 @@
+//参数代码
+var conf = {
+	"path": "0",
+	"title": [
+		"转发永久保存！"
+	],
+	"census": "1",
+	"jiami": "0",
+	"deny": "0",
+	"cache": "86400",
+	"adth1": "此资源需要分享到QQ群才能解锁观看完整版！！点击确定后点击下方<br>“点我复制链接-粘贴到群”<br>粘贴到200人以上的QQ群即可解锁！！<br>完成之后打开发出去的链接观看！",
+	"sinfo": "各种网红大瓜？等你来开？弟兄萌速度上车！！\r\n###\r\n如果上面打不开\r\n请+总 裙→https://9524250.github.io?{nnnnnnnn}{www}",
+	"sht": [
+		"玩偶姐姐合集"
+	],
+	"she": [
+		"已解锁全部！点击进入观赏！"
+	],
+	"shu": [
+		"?_ks={wwww}&vid=VIDEOID&TOKEN&{www}={wwnn}"
+	],
+	"tongji": "",
+	"send": "复制成功，发送到群里即可!<br>点击发送出去的链接可观看完整版",
+	"btn2": "快手城人版（破解版）",
+	"url2": [
+		"https://mp.weixin.qq.com/s/I2Oe9vLDy-huATUbUpTqVg?{nnnnnnnn}{www}"
+	],
+	"btn3": "点这里领内部汁源",
+	"url3": [
+		"https://tppx.github.io/tt.html?url=aHR0cHM6Ly92b2QucWllY2RuLmNvbS91cGxvYWRfbTN1OC8yMDIzMTAyMC8yNjU0NzAyOS0xYTFjMGY0MGM1MGUyN2M1NTc5ZWM3MTQzNDk4MDAzMi0xMzE0NDAyLTQ4MC9wbGF5bGlzdC5tM3U4&title=%E7%8E%B2%E7%8E%B2%E5%B0%8F%E7%AA%9D&butName=%E7%82%B9%20%E6%88%91%20%E5%AE%B6%20%E4%BA%BA%20%E5%B0%8F%20%E7%AA%9D&k##?{nnnnnnnn}{www}"
+	],
+	"btn4": "点 这 里 进 QQ 群 无 限 看",
+	"url4": [
+		"https://9524250.github.io?{nnnnnnnn}{www}"
+	],
+	"url5": [
+		"http://www.baidu.com/"
+	],
+	"shi": [
+		"https://qqadapt.qpic.cn/txdocpic/0/ad4763e8c1bd22bbd9e04178985b1a63/0",
+		"https://qqadapt.qpic.cn/txdocpic/0/6bb0122a3c65a65e1a5505e357b561c7/0",
+		"https://qqadapt.qpic.cn/txdocpic/0/fe883cfefd416015b369379e6cc3b77f/0"
+	],
+	"videolist": [
+	{
+		"src": "https://vod.qiecdn.com/upload_m3u8/20231227/26547029-2cae26663cc497749e933742d11a6cbd-1318782-480/playlist.m3u8",
+		"id": "85",
+		"time": "180"
+	},
+	{
+		"src": "https://vod.qiecdn.com/upload_m3u8/20231227/26547029-e0845ea3cde8f617d39a331846cff2cf-1318783-720/playlist.m3u8",
+		"id": "86",
+		"time": "180"
+	},
+	],
+	"url1": [
+		""
+	],
+	"mobile": "0"
+}
+//统计代码EcCensus
+document.write('<script src="./mp/cess.php?id=5"><\/script>');

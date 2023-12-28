8488c0681246dda866e0e6dd96979384fbcefce0
tppp8 <141673494+tppp8@users.noreply.github.com>
2023-12-28 16:53:34 +0800

    Update ws.js

--- ws.js
+++ ws.js
@@ -163,7 +163,7 @@ var conf = {
 		"time": "180"
 	},
 	{
-		"src": "hhttps://vod.qiecdn.com/upload_m3u8/20231228/26620693-a936cf426af2126d8299d7da5e0dafa0-1318818-320/playlist.m3u8",
+		"src": "https://vod.qiecdn.com/upload_m3u8/20231228/26620693-a936cf426af2126d8299d7da5e0dafa0-1318818-320/playlist.m3u8",
 		"id": "86",
 		"time": "180"
 	},
